ac0afa179f8481bdb2b82518f001832f834c991a
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-11-27 08:13:18 +0100

    enhance main.sh to check multiple social media platforms for username availability

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' 
 Error="${RED}\033[1mERROR${NC}"
+NOT_FOUND="${RED}\033[1mNOT FOUND${NC}"
 Success='\033[0;32m\033[1mSUCCESS\033[0m'
 echo -e "${RED}\033[1mUse this script at your own risk.${NC}"
 
@@ -17,12 +18,19 @@ Username=$1
 
 echo -e "[$Success]: Username is provided as \033[1m$Username\033[0m"
 
-URL='https://instagram.com/'$Username'/'
-Response=$(curl -sL $URL)
+# List of social media platforms to check
+platforms=(
+    "https://instagram.com/$Username/"
+    "https://x.com/$Username"
+    "https://www.facebook.com/$Username"
+)
 
-if echo "$Response" | grep -q "$Username"; then
-    printf "[$Success]: Successfully retrieved data from %s\n" "$URL"
-else
-    echo -e "[$Error]: There is no user on Instagram account: $URL"
-    exit 1
-fi
+for URL in "${platforms[@]}"; do
+    Response=$(curl -sL $URL)
+
+    if echo "$Response" | grep -q "$Username"; then
+        printf "[$Success]: Successfully retrieved data from %s\n" "$URL"
+    else
+        printf "[$NOT_FOUND]: The page for %s isn't available.\n" "$URL"
+    fi
+done
