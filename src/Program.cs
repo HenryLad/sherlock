862527c8cfe53aa4eba2cd336a83074140f3c96d
Henry Ladstätter <henry.ladstaetter08@gmail.com>
2024-12-02 21:38:22 +0100

    add error handling for URL input and display links from multiple URLs

--- src/Program.cs
+++ src/Program.cs
@@ -3,18 +3,34 @@ using HtmlAgilityPack;
 
 int MAX_AMOUNT_OF_LINKS = args.Length - 1;
 
+if (args.Length == 0)
+{
+    Console.WriteLine("No arguments provided.");
+    return;
+}
+
 string username = args[0];
 Console.WriteLine($"Mark des Beidel hat den username: {username}");
 
-string url = args[1];
-HtmlWeb web = new HtmlWeb();
-HtmlDocument doc = web.Load(url);
-
-
-foreach (HtmlNode link in doc.DocumentNode.SelectNodes("//a[@href]"))
+for (int i = 1; i < args.Length; i++)
 {
-   HtmlAttribute attr = link.Attributes["href"];
-   Console.WriteLine(attr.Value);
+    string url = args[i];
+    HtmlWeb web = new HtmlWeb();
+    HtmlDocument doc = web.Load(url);
+
+    var nodes = doc.DocumentNode.SelectNodes("//a[@href]");
+    if (nodes != null)
+    {
+        foreach (HtmlNode link in nodes)
+        {
+            HtmlAttribute attr = link.Attributes["href"];
+            Console.WriteLine(attr.Value);
+        }
+    }
+    else
+    {
+        Console.WriteLine($"No links found on {url}");
+    }
 }
 
 Console.WriteLine("Program finished");
